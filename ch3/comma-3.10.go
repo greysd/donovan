33ab9afa624f3cf2bc871caa525f291d29955005
Sergey Davydov <oae2@mail.ru>
2022-09-19 00:11:52 +0300

    add non-recursive comma function

--- ch3/comma-3.10.go
+++ ch3/comma-3.10.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func comma(s string) string {
-
+	n := len(s)
+	st := s[:n%3]
+	for i := 0; i < n-2; i = i + 3 {
+		st = st + "," + s[n%3+i:n%3+i+3]
+	}
+	return strings.TrimPrefix(st, ",")
 }
 
 func main() {
-	fmt.Println(comma("12345"))
+	fmt.Println(comma("1234567890123456789"))
 }
