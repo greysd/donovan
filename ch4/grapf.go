b0c20df48f652879d4261526d6c7f843b3fcc840
Sergey Davydov <oae2@mail.ru>
2022-09-23 11:59:33 +0300

    i

---
+++ ch4/grapf.go
@@ -0,0 +1,17 @@
+package main
+
+func addEdge(from, to string) {
+	edges := graph[from]
+	if edges == nil {
+		edges = make(map[string]bool)
+		graph[from] = edges
+	}
+	edges[to] = true
+}
+
+func hasEdge(from, to string) bool {
+	return graph[from][to]
+}
+func main() {
+
+}
